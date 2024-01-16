19bd150c301b99dee91015873d12850efb0b85d5
alsetsoft-liubomyr <alsetsoft@gmail.com>
2024-01-16 22:45:12 +0200

    fixed issues

--- js/main.js
+++ js/main.js
@@ -65,33 +65,33 @@ updateSlide();
 let slides2 = [
   {
     title: "CoolFlow. Solution for retail merchandising",
-    text: `<b>Business Need:</b> Automating the workflow of merchandisers and controlling refrigerator store placements to enhance product presentation and maximize sales.
-    <br><br><b>Solution:</b> An Innovative IoT Solution includes a Cross-Platform app paired with smart sensors in refrigerators, transmitting data to our web servers. The app displays this data through an intuitive interface, offering real-time monitoring and insightful analytics. 
-    <br><br><b>Value Delivered:</b> Solution provides inventory management in retail, provides real-time stock and product rotation insights, ensures planogram compliance, and reduces merchandising costs, thereby boosting efficiency and profitability in the retail sector.`,
+    text: `<b style="font-weight: 500;">Business Need:</b> Automating the workflow of merchandisers and controlling refrigerator store placements to enhance product presentation and maximize sales.
+    <br><b style="font-weight: 500;">Solution:</b> An Innovative IoT Solution includes a Cross-Platform app paired with smart sensors in refrigerators, transmitting data to our web servers. The app displays this data through an intuitive interface, offering real-time monitoring and insightful analytics. 
+    <br><b style="font-weight: 500;">Value Delivered:</b> Solution provides inventory management in retail, provides real-time stock and product rotation insights, ensures planogram compliance, and reduces merchandising costs, thereby boosting efficiency and profitability in the retail sector.`,
   },
   {
     title: "Aloto. Online marketplace",
-    text: `<b>Business Need:</b> Local buyers and sellers require a simplified and secure platform that can facilitate seamless peer-to-peer transactions, eliminating the need for unnecessary intermediaries and fostering community engagement.
-    <br><br><b>Solution:</b> The Cross-Platform platform is designed to be user-friendly and accessible, featuring easy listing of items for sale. Additionally, an Admin panel is developed for store owners to manage the platform and address security challenges.
-    <br><br><b>Value Delivered:</b> The platform offers multiple revenue streams including advertisement and premium listings. Users benefit from economic opportunities by conveniently buying or selling goods and services within their community.`,
+    text: `<b style="font-weight: 500;">Business Need:</b> Local buyers and sellers require a simplified and secure platform that can facilitate seamless peer-to-peer transactions, eliminating the need for unnecessary intermediaries and fostering community engagement.
+    <br><b style="font-weight: 500;">Solution:</b> The Cross-Platform platform is designed to be user-friendly and accessible, featuring easy listing of items for sale. Additionally, an Admin panel is developed for store owners to manage the platform and address security challenges.
+    <br><b style="font-weight: 500;">Value Delivered:</b> The platform offers multiple revenue streams including advertisement and premium listings. Users benefit from economic opportunities by conveniently buying or selling goods and services within their community.`,
   },
   {
     title: "Caregivers Management Solution",
-    text: `<b>Business Need:</b> Streamlining real time caregiver management solution in healthcare is essential for consistent, high-quality patient care, requiring adherence to HIPAA and industry standards.
-    <br><br><b>Solution:</b> System grant caregiver management, with a mobile app for schedule access, task tracking, and compliance updates. Prioritizing HIPAA adherence, it ensures data security and simplifies administration, enhancing healthcare efficiency and quality.
-    <br><br><b>Value Delivered:</b> Delivers substantial value by streamlining the administrative processes related to caregiving. It ensures that caregivers are well-coordinated, compliant with regulations, and have real-time access to essential information. App is used over many caregivers clinics in USA.`,
+    text: `<b style="font-weight: 500;">Business Need:</b> Streamlining real time caregiver management solution in healthcare is essential for consistent, high-quality patient care, requiring adherence to HIPAA and industry standards.
+    <br><b style="font-weight: 500;">Solution:</b> System grant caregiver management, with a mobile app for schedule access, task tracking, and compliance updates. Prioritizing HIPAA adherence, it ensures data security and simplifies administration, enhancing healthcare efficiency and quality.
+    <br><b style="font-weight: 500;">Value Delivered:</b> Delivers substantial value by streamlining the administrative processes related to caregiving. It ensures that caregivers are well-coordinated, compliant with regulations, and have real-time access to essential information. App is used over many caregivers clinics in USA.`,
   },
   {
     title: "Clean energy certificates trading solution",
-    text: `<b>Business Need:</b> Solution was needed to efficiently manage energy certificate trading, representing renewable energy generation. Current processes lack transparency and are manual, requiring a simplified approach for compliance and market efficiency.
-    <br><br><b>Solution:</b> Created a digital WEB platform for energy certificate trading, automating and streamlining the process. It ensures tracking, validation, and transparency through secure, real time, relational databases, enhancing efficiency and regulatory compliance.
-    <br><br><b>Value Delivered:</b> Solution simplifies and secure energy certificate trading for companies that buy and sell clean energy, it fostering sustainability and trust among stakeholders.`,
+    text: `<b style="font-weight: 500;">Business Need:</b> Solution was needed to efficiently manage energy certificate trading, representing renewable energy generation. Current processes lack transparency and are manual, requiring a simplified approach for compliance and market efficiency.
+    <br><b style="font-weight: 500;">Solution:</b> Created a digital WEB platform for energy certificate trading, automating and streamlining the process. It ensures tracking, validation, and transparency through secure, real time, relational databases, enhancing efficiency and regulatory compliance.
+    <br><b style="font-weight: 500;">Value Delivered:</b> Solution simplifies and secure energy certificate trading for companies that buy and sell clean energy, it fostering sustainability and trust among stakeholders.`,
   },
   {
     title: "Marine traveling optimization system",
-    text: `<b>Business Need:</b> Integrated solution that addresses the various pain points experienced by employees during maritime travel. This solution aim to enhance the overall travel experience by focusing on aspects such as real time monitoring, ease of booking, onboard safety, connectivity, entertainment, and personalized services.
-    <br><br><b>Solution:</b> Web platform streamlines real time monitoring, booking, safety, and amenities for maritime travel. It additionally provides customized certificates issuance and features related to video courses.
-    <br><br><b>Value Delivered:</b> This solution enables companies to give more safety for their employees while working at sea, offering connectivity, real-time information, communication and employee well-being.`,
+    text: `<b style="font-weight: 500;">Business Need:</b> Integrated solution that addresses the various pain points experienced by employees during maritime travel. This solution aim to enhance the overall travel experience by focusing on aspects such as real time monitoring, ease of booking, onboard safety, connectivity, entertainment, and personalized services.
+    <br><b style="font-weight: 500;">Solution:</b> Web platform streamlines real time monitoring, booking, safety, and amenities for maritime travel. It additionally provides customized certificates issuance and features related to video courses.
+    <br><b style="font-weight: 500;">Value Delivered:</b> This solution enables companies to give more safety for their employees while working at sea, offering connectivity, real-time information, communication and employee well-being.`,
   },
 ];
 
@@ -133,36 +133,42 @@ updateSlide2();
 // слайдер 3
 let slides3 = [
   {
-    img: "images/image4.png",
-    name: `<a href="https://www.linkedin.com/company/alsetsoft">Liubomyr</a>`,
+    img: "images/Team.png",
+    name: `Liubomyr`,
     position: "CEO",
     text: `Started career in 2016 as a full-stack software engineer, and in 2023, took the
     leadership helm at Alsetsoft. <br>With a keen interest in both technology and business development, he
     assists
     customers in envisioning and planning comprehensive strategies to enhance their business through
     innovative technologies. <br>Collaborating closely with the Alsetsoft team, he turns these visions into
-    reality, delivering tangible improvements and value to clients.`,
+    reality, delivering tangible improvements and value to clients.
+    <a href="https://www.linkedin.com/in/liubomyr-liubchynskyi-a50111144" class="link-block-6-copy w-inline-block">
+    <div>in</div>
+    </a>`,
   },
   {
-    img: "images/image4.png",
+    img: "images/Team.png",
     name: "Oleh",
     position: "CTO",
-    text: "Agility. Initiation. Planning. Design. Implementation. Closure. It is our workflow and how we do our job to produce the best results at the best time for our customers.",
+    text: `Oleh  is a seasoned professional with a comprehensive background in web and mobile development, primarily using Microsoft technologies.
+    His expertise extends to development cloud solutions, managing large-scale projects, and implementing various types of architectures.
+    Beyond his technical proficiency, he has a profound passion for the fields of IoT and Robotics systems, reflecting a forward-thinking approach to emerging technologies.
+    Throughout his career, he has successfully managed over 30 software engineers, playing a pivotal role in significantly enhancing their skills and professional development.`,
   },
   {
-    img: "images/image4.png",
+    img: "images/Team.png",
     name: "Slide3",
     position: "CEO",
     text: "Agility. Initiation. Planning. Design. Implementation. Closure. It is our workflow and how we do our job to produce the best results at the best time for our customers.",
   },
   {
-    img: "images/image4.png",
+    img: "images/Team.png",
     name: "Slide4",
     position: "CEO",
     text: "Agility. Initiation. Planning. Design. Implementation. Closure. It is our workflow and how we do our job to produce the best results at the best time for our customers.",
   },
   {
-    img: "images/image4.png",
+    img: "images/Team.png",
     name: "Slide5",
     position: "CEO",
     text: "Agility. Initiation. Planning. Design. Implementation. Closure. It is our workflow and how we do our job to produce the best results at the best time for our customers.",
